6ca716836d20d0cf92c06171fae82a24b5ff09c8
Dave March <dbmarch@gmail.com>
2026-05-29 16:58:34 -0400

    Add stars

--- coursework/rating-ui/src/App.jsx
+++ coursework/rating-ui/src/App.jsx
@@ -1,12 +1,8 @@
 import Rating from "./components/Ratings";
 
 const App = () => {
-  const name = "Dave";
   return (
     <>
-      <h2>Welcome {name}</h2>
-      <Rating />
-      <Rating />
       <Rating />
     </>
   );
--- coursework/rating-ui/src/components/Ratings.jsx
+++ coursework/rating-ui/src/components/Ratings.jsx
@@ -1,13 +1,30 @@
 const Rating = () => {
-  return <div style={styles.container}>Rate Your Experience!</div>;
+  //   return <div style={styles.container}>Rate Your Experience!</div>;
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+  const clicked = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div className="rating-container">
+      <h2>Rate Your Experience!</h2>
+      <div className="stars">
+        {stars.map((star) => (
+          <span key={star} onClick={clicked}>
+            {"\u2605"}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-const styles = {
-  container: {
-    textAlign: "center",
-    fontFamily: "Arial, sans-serif",
-    padding: "20px",
-  },
-};
+// const styles = {
+//   container: {
+//     textAlign: "center",
+//     fontFamily: "Arial, sans-serif",
+//     padding: "20px",
+//   },
+// };
 
 export default Rating;
